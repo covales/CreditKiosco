85fe06cf8ed564a5c7708d741dd0093a4de4a933
covales <covales.a.15@gmail.com>
2021-04-28 18:22:24 -0400

    nuevo comienzo

--- app/src/main/java/com/example/creditokiosco/MainActivity.java
+++ app/src/main/java/com/example/creditokiosco/MainActivity.java
@@ -9,18 +9,12 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView fecha;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Date d=new Date();
 
-        //obtener la fecha
-        fecha = (TextView)findViewById(R.id.txvFecha);
-        SimpleDateFormat fecc = new SimpleDateFormat("d,MMMM,YYYY");
-        String fechaComString = fecc.format(d);
-        fecha.setText(fechaComString);
     }
 }
